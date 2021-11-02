0d98faa6aed2bd9e068560291f67069aa69d5481
Rubén <gonzru01atwork@gmail.com>
2021-11-02 17:12:46 -0500

    fix navbar bug on phones

--- js/script.js
+++ js/script.js
@@ -14,11 +14,14 @@ const myFunction = () => {
 let prevScrollpos = window.scrollY;
 
 window.onscroll = () => {
+	let navbar = document.getElementById("navbarid");
 	let currentScrollpos = window.scrollY;
 	if (prevScrollpos > currentScrollpos) {
 		document.getElementById("navbarid").style.top = "0";
+		navbar.className = "navbar";
 	} else {
 		document.getElementById("navbarid").style.top = "-100px";
+		navbar.className = "navbar";
 	}
 	prevScrollpos = currentScrollpos;
 }
